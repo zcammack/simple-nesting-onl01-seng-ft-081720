3c0f38017e836e1f15f9804da388f517391ef9cf
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 16:46:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+				:yukihiro_matsumoto => {
+					:known_for => "Ruby",
+					:languages => ["LISP", "C"]
+				}
+				}
      }
 
 
