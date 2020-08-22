c7df55e8f88337b2f0338e4267cfbb582113fe2f
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 16:48:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,10 +104,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-
-
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
