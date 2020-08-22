136b70fecd8f2e3808ebbcac4028fc800eb86f4d
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 16:44:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages[0]]
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
