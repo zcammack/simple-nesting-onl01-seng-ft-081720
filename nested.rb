867410c6d9b58f6b49d1d889fb53c1ce1f5fa508
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 16:49:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 programmer_hash[:alan_kay][:known_for] => "GUI"
 end
 
 def adding_to_dennis
